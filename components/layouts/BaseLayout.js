0da9ae65c085ddd6ca117bafc771b57ddd60f365
Or Hasson <53047660+hassonor777@users.noreply.github.com>
2020-01-28 18:40:14 +0200

    styling...

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -3,13 +3,16 @@ import React from "react";
 
 
 
-const BaseLayout = (props) =>{
-    return(
-        <React.Fragment>
-            <Header/>
-            {props.children}
-        </React.Fragment>
-    )
+const BaseLayout = props => {
+    const { className, children } = props;
+    return (
+        <div className="layout-container">
+            <Header />
+            <main className={`cover ${className}`}>
+                <div className="wrapper">{children}</div>
+            </main>
+        </div>
+    );
 };
-
 export default BaseLayout;
+
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,90 @@
 import React from 'react';
 import BaseLayout from "../components/layouts/BaseLayout";
-import { Button, Container  } from 'reactstrap';
+import ReactTyped from 'react-typed';
+import { Button, Container,Row,Col} from 'reactstrap';
 
 
 
 
 class Index extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isFlipping: false
+        }
+
+        this.roles = ['Developer', 'Tech Lover', 'Team Player', 'React.js', 'Next.js', 'Node.js','JAVA','And More...'];
+    }
+    componentDidMount() {
+        this.animateCard();
+    }
+
+    componentWillUnmount() {
+        this.cardAnimationInterval && clearInterval(this.cardAnimationInterval);
+    }
+
+    animateCard() {
+        this.cardAnimationInterval = setInterval(() => {
+            this.setState({
+                isFlipping: !this.state.isFlipping
+            });
+        }, 60000);
+    }
 
     render() {
+        const { isFlipping } = this.state;
         return (
-            <BaseLayout>
-                <Container>
+            <BaseLayout className="cover">
+                <div className="main-section">
+                    <div className="background-image">
+                    </div>
+                    <Container>
+                        <Row>
+                            <Col md="6">
+                                <div className="hero-section">
+                                    <div className={`flipper`}>
+                                        <div className="back">
 
-                </Container>
+                                            <div className="hero-section-content">
+
+                                                <div className="hero-section-content-intro">
+                                                    <h2> Full Stack - Or Hasson </h2>
+                                                    Have a look at my site.
+                                                </div>
+                                            </div>
+                                            <div className="shadow-custom">
+                                                <div className="shadow-inner"> </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col md="6" className="hero-welcome-wrapper">
+                                <div className="hero-welcome-text">
+                                    <h1>
+                                        Welcome to the portfolio website of Or Hasson :)!
+                                    </h1>
+                                </div>
+                                <ReactTyped
+                                    loop
+                                    typeSpeed={60}
+                                    backSpeed={60}
+                                    strings={this.roles}
+                                    backDelay={900}
+                                    loopCount={0}
+                                    showCursor
+                                    className="self-typed"
+                                    cursorChar="|"
+                                />
+                                <div className="hero-welcome-bio">
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             </BaseLayout>
+
         )
     }
 }
