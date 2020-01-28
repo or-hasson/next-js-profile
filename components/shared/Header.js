6e7ef99b440d0572bda93cbd9a7d2e9001c23db2
Or Hasson <53047660+hassonor777@users.noreply.github.com>
2020-01-28 19:23:47 +0200

    Header styling...

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,28 +1,125 @@
-import Link from "next/link";
+import React from 'react';
+import ActiveLink from '../ActiveLink';
+import '../../styles/_header.scss'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    Dropdown,
+    DropdownItem,
+    DropdownToggle,
+    DropdownMenu} from 'reactstrap';
 
 
+const BsNavLink = (props) => {
+    const { route, title } = props;
+    const className = props.className || "";
 
-const Header = (props) =>{
-    return(
-        <React.Fragment>
-            {props.children}
-            <Link href="/">
-                <a>Home </a>
-            </Link>
-            <Link href="/about">
-                <a>about </a>
-            </Link>
-            <Link href="/cv">
-                <a>cv </a>
-            </Link>
-            <Link href="/blogs">
-                <a>blogs </a>
-            </Link>
-            <Link href="/portfolios">
-                <a>portfolios </a>
-            </Link>
-        </React.Fragment>
+    return (
+        <ActiveLink activeClassName="active" route={route}>
+            <a className={`nav-link port-navbar-link ${className}`}> {title} </a>
+        </ActiveLink>
     )
 }
 
-export default Header;
+
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false,
+            dropdownOpen: false
+        };
+
+        this.toggle = this.toggle.bind(this);
+        this.toggleDropdown = this.toggleDropdown.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    toggleDropdown() {
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+        });
+    }
+
+    renderBlogMenu() {
+            return (
+                <Dropdown className="port-navbar-link port-dropdown-menu" nav isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+                    <DropdownToggle className="port-dropdown-toggle" nav caret>
+                        Blog
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem>
+                            <BsNavLink className="port-dropdown-item"
+                                       route="/blogs"
+                                       title="Blogs" />
+                        </DropdownItem>
+                        <DropdownItem>
+                            <BsNavLink className="port-dropdown-item"
+                                       route="/blogs/new"
+                                       title="Create a Blog" />
+                        </DropdownItem>
+                        <DropdownItem>
+                            <BsNavLink className="port-dropdown-item"
+                                       route="/blogs/dashboard"
+                                       title="Blogs Dashboard" />
+                        </DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+            )
+
+
+        return (
+            <NavItem className="port-navbar-item">
+                <BsNavLink route="/blogs" title="Blog" />
+            </NavItem>
+        )
+    }
+
+    render() {
+        const { className } = this.props;
+        const { isOpen } = this.state;
+
+        const menuOpenClass = isOpen ? 'menu-open' : 'menu-close';
+
+        return (
+            <div>
+                <Navbar className={`port-navbar port-nav-base absolute ${className} ${menuOpenClass}`} color="transparent" dark expand="md">
+                    <NavbarBrand className="port-navbar-brand" href="/">Or Hasson</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem className="port-navbar-item">
+                                <BsNavLink route="/" title="Home" />
+                            </NavItem>
+                            <NavItem className="port-navbar-item">
+                                <BsNavLink route="/about" title="About" />
+                            </NavItem>
+                            <NavItem className="port-navbar-item">
+                                <BsNavLink route="/portfolios" title="Portfolio" />
+                            </NavItem>
+                            {this.renderBlogMenu()}
+                            <NavItem className="port-navbar-item">
+                                <BsNavLink route="/cv" title="Cv" />
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+
+
+
